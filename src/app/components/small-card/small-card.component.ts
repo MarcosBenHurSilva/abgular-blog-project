24f1ddb8f573cf92369666a80d14813acf6649f5
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-31 03:09:07 -0300

    update

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
 export class SmallCardComponent {
-
+  cardDate = new Date('2023-12-28'); // Example date
 }
